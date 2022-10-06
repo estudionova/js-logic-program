5fee3637a9f2cb91040f85c72d866c46b34843aa
Reinaldo Junior <rsljunior@Live.com>
2022-10-06 19:26:27 -0300

    operadores logicos

---
+++ 02-operadores-logicos.js
@@ -0,0 +1,30 @@
+const usuario = 'Reinaldo';
+const senha = 123456;
+
+//                   true                     false
+const vaiLogar = usuario === 'Reinaldo' && senha === 12345;
+console.log(vaiLogar);
+
+
+//                   true                     false
+const vaiLogarOr = usuario === 'Reinaldo' || senha === 12345;
+console.log(vaiLogarOr);
+
+
+
+
+
+/*
+
+const expressaoAnd = true && true && true;
+const expressaoOr = true || false;
+
+console.log(expressaoAnd);
+console.log(expressaoOr);
+
+Operadores Lógicos
+&& -> AND -> E: Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU : 
+! -> NOT -> NÃO
+
+*/
