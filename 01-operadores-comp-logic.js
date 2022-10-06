9c8a98c62d5013fd4ec660b28f9ad935af83298b
Reinaldo Junior <rsljunior@Live.com>
2022-10-06 17:09:24 -0300

    operadores de comparação

---
+++ 01-operadores-comp-logic.js
@@ -0,0 +1,42 @@
+
+const num1 = 10;
+const num2 = '10';
+const comp = num1 !== num2;
+console.log(comp);
+
+
+
+/*
+
+const num1 = 10; // number
+const num2 = '10'; // string
+
+console.log( num1 === num2); // iguais com == usasse === , correção de tipos (converter um tipo em outro)
+
+
+
+console.log(10 > 5);
+console.log(10 >= 5);
+console.log(10 < 5);
+console.log(10 <= 5);
+console.log(10 == 5);
+
+const comp = 10 > 5;
+console.log(comp);
+
+
+
+Operadores de comparação
+> maior que
+>= maior ou igual a
+< menor que
+<=menor que ou igual a
+== igualdade(valor) ***************** ( não é recomendavel usar )
+===igualdade estrita ( valor e tipo )
+!= diferente (valor) ***************** ( não é recomendavel usar )
+!== diferente estrito (valor e tipo)
+
+
+
+
+*/
