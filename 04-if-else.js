dbb2169fa5f7531cb8bd145aeb5dfc076ab6da25
Reinaldo Junior <rsljunior@Live.com>
2022-10-09 09:53:37 -0300

    if else

---
+++ 04-if-else.js
@@ -0,0 +1,58 @@
+
+const tenhoGrana = false;
+
+if(tenhoGrana ){
+  console.log('vou sair de casa');
+}
+else{
+  console.log('não vou sair de casa');
+}
+
+
+
+
+
+/*
+entre 0 - 11 -> bom dia
+entre 12 - 17 -> boa tarde
+entre 18 - 23 -> boa noite
+
+// if pode ser usado sozinho
+// sempre que utilizo a palavra else, precisa ter um if (sempre inicia com if)
+// pode ter varios else ifs na checagem (condição)
+// só pode ter um else na checagem
+// podemos usar condições sem else if, utilizando apenas if e else
+
+
+const hora = 13;
+
+if (hora >= 0 && hora <=11) {
+  console.log(`Bom dia a hora atual é ${hora}:00`)
+  
+} else if (hora >= 12 && hora <=17){
+  console.log(`Boa tarde a hora atual é ${hora}:00`)
+}
+else if (hora >= 18 && hora <=23) {
+  console.log(`Boa noite a hora atual é ${hora}:00`)
+}
+else{
+  console.log('Olá');
+  console.log(`hora invalida ->  ${hora}`)
+}
+
+
+
+
+
+const hora = 13;
+
+if (hora <= 12) {
+  console.log(`Bom dia, hora atual é ${hora}:00`);
+
+  console.log(1 + 1);
+}
+
+
+
+
+*/
