0b6f4950bfc57198029a05621e05756f68d813a1
Reinaldo Junior <rsljunior@Live.com>
2022-10-10 20:45:04 -0300

    mais if else

--- 04-if-else.js
+++ 04-if-else.js
@@ -1,3 +1,45 @@
+const numero = 10;
+
+
+if (numero >= 0 && numero <= 5) {
+  console.log('o numero está entre 0 e 5');
+}
+
+else if (numero >= 6 && numero <= 8) {
+  console.log('o numero está entre 6 e 8');
+
+}
+
+/*
+else if ( 1 === 1){
+  console.log('O numero é igual');
+} //saiu do else if
+*/
+else if (numero >= 9 && numero <= 11) {
+  console.log('o numero está entre 9 e 11');
+
+}
+
+else {
+  console.log(`O numero é ${numero}`);
+}
+
+
+
+
+/*
+
+const numero = 10;
+
+// Se isso ocorrer, faça isso (if) 
+// se não faça isso (else)
+if (numero >= 0 && numero <= 5) {
+  console.log('o numero está entre 0 e 5');
+}
+else {
+  console.log(`O numero não está entre 0 e 5 -> o numero é ${numero}`);
+}
+
 
 const tenhoGrana = false;
 
@@ -8,11 +50,6 @@ else{
   console.log('não vou sair de casa');
 }
 
-
-
-
-
-/*
 entre 0 - 11 -> bom dia
 entre 12 - 17 -> boa tarde
 entre 18 - 23 -> boa noite
