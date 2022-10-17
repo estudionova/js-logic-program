fbfb9e9ee3464c9e0f0da849c934c933e531fd40
Reinaldo Junior <rsljunior@Live.com>
2022-10-17 19:28:52 -0300

    Atribuição x Desestruturação com objetos

---
+++ 10-atribui-x.desestru.obj.js
@@ -0,0 +1,19 @@
+const pessoa = {
+  nome: 'Reinaldo',
+  sobrenome: 'Junior',
+  idade: 36,
+  endereco: {
+    rua: 'pedro gama',
+    numero: 320
+  }
+};
+
+//const nome = pessoa.nome;
+
+//atribuição via desestruturação
+//const { nome = '', sobrenome, idade , endereco: { rua: r = '12345', numero}, endereco } = pessoa;
+//console.log(nome, sobrenome, idade, r, numero, endereco);
+
+const { nome, sobrenome,  ...resto} = pessoa;
+
+console.log(nome, sobrenome, resto);
