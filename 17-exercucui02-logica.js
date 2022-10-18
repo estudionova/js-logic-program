e1c2db6ab1a041b451499cc488e3cd8536069181
Reinaldo Junior <rsljunior@Live.com>
2022-10-18 19:54:57 -0300

    exercicio 2 logica

---
+++ 17-exercucui02-logica.js
@@ -0,0 +1,33 @@
+/* 
+Escreva uma função chamada ePaisagem que recebe dois argumentos, largura e altura
+de uma imagem ( number ).
+retorne true se a imagem estiver no modo paisagem
+*/
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1920, 1080));
+
+
+
+
+
+/*
+
+
+ - - - - - - - - - - - - - - - - - - - - - 
+
+function ePaisagem(largura, altura) {
+  return largura > altura;
+}
+
+ - - - - - - - - - - - - - - - - - - - - - 
+
+function ePaisagem(largura, altura) {
+  return largura > altura ? true : false;
+
+}
+
+console.log(ePaisagem(1080, 1920));
+
+*/
