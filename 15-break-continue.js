a2612717305241fc9912233f349ee0aa07a3d174
Reinaldo Junior <rsljunior@Live.com>
2022-10-18 19:37:45 -0300

    break e continue

---
+++ 15-break-continue.js
@@ -0,0 +1,22 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let numero of numeros) {
+
+  if(numero === 7){
+    console.log('7 encontrado, saindo...');
+    break;
+  }
+
+
+
+
+  /*
+  if (numero === 2) {
+    console.log('pulou o numero 2');
+    continue;
+  
+  }
+  */
+
+  console.log(numero);
+}
